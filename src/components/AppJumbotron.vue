89eb24851a04a47bdbbb9c8bff32b0ac5dcc8f85
PasquiFra <francesco.pasquinoni@gmail.com>
2024-01-31 17:36:53 +0100

    jumbotron with dynamic method

--- src/components/AppJumbotron.vue
+++ src/components/AppJumbotron.vue
@@ -3,12 +3,18 @@ export default {
     name: "AppJumbotron",
     data: () => ({
         image: "jumbo.jpg"
-    })
+    }),
+    methods: {
+        imagePath(image) {
+            const url = new URL(`../assets/${image}`, import.meta.url);
+            return url.href;
+        }
+    }
 }
 </script>
 
 <template>
-    <img src="../../public/jumbo.jpg" alt="jumbo">
+    <img :src="imagePath(image)" alt="jumbo">
 </template>
 
 <style></style>
